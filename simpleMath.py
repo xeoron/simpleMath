797e793d9de3a60e480174a4ba3a5f8cf1a51c0f
Jason Campisi <aitsinformation@gmail.com>
2022-08-02 09:06:53 -0400

    nuts and bolts
    just simplifying things

--- simpleMath.py
+++ simpleMath.py
@@ -2,8 +2,8 @@
 # Name: simpleMath.py
 # License: Released under GPL v2 or higher. Details here http://www.gnu.org/licenses/gpl.html
 # Author: Jason Campisi
-# Version 1.2.1
-# Date: 7/17/2022
+# Version 1.3.0
+# Date: 8/02/2022
 # Purpose: Send some math to the program at the command line to calulate & format it with commas.
 # Usage: simpleMath.py "insert equation here"
 #  So I don't have to do this: echo "50.2*8*260" | bc | xargs /usr/bin/printf "%'d\n" 2>/dev/null
@@ -18,8 +18,8 @@ numbers = sys.argv[1]
 print ("This %s yields" % numbers)
 
 try:
-    result = eval(numbers)
-    print ("     %s" % addFormatting(result))
+    #result = eval(numbers)
+    print ("     %s" % addFormatting(eval(numbers)))
 except SyntaxError:
     print ("Failure: %s" % SyntaxError)    
     exit()
