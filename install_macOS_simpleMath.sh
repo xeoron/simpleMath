972e201cb5f1b0c09f3155fda2c4397fc49d7749
Jason Campisi <aitsinformation@gmail.com>
2024-07-18 14:49:13 -0400

    installers
    MacOS and Linux

--- install_macOS_simpleMath.sh
+++ install_macOS_simpleMath.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 #Author: Jason Campisi
-#Date: 7/17/2024
-#version 0.2.6
+#Date: 9/25/2016
+#version 0.2.5 macOS
 #Released under the GPL v2 or higher
 NAME="simpleMath"
 EXT="py"
 FILE="$NAME.$EXT"
-LOCATION="/usr/bin"
+LOCATION="/opt/local/bin/"
 echo "$FILE installer:";
 
  echo " Checking if '$FILE' exists in the current folder..."
@@ -22,18 +22,9 @@ echo "$FILE installer:";
 echo " Setting file to executable...";
 chmod +x ./$FILE
 
-echo " Checking for Perl at /usr/bin/perl ...";
-if [ "$(whereis perl)" != '/usr/bin/perl' ]; then
-	echo " Error: Perl can not be found.";
-	echo " ->If it is installed, then make a symlink that points /usr/bin/perl to the right location, else please install it.";
-	exit 1;
-else 
-	echo " ...Perl found!";
-fi
-
 echo " Checking if you have the clearance to install this ...";
 if [ "$(whoami)" != 'root' ]; then
-	echo " You do not have permission to install ./$FILE";
+	echo " You do not have permission to install './$FILE'";
 	echo " ->You must be a root user.";
 	echo " ->Try instead: sudo $0";
 	exit 1;
@@ -41,8 +32,6 @@ else
 	echo " Root access granted for $0";	
 fi
 
-echo " Installing $NAME to $LOCATION/$NAME ...";
-cp ./$FILE $LOCATION/$NAME
+echo " Installing $NAME to $LOCATION ...";
+cp ./$FILE $LOCATION$NAME
 echo " Setup complete."
-echo "Testing install with this command\n>$NAME --version";
-$NAME --version
